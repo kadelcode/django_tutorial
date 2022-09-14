22615add97120b17fec7390b8ae520adaf2d27b9
kadelcode <kadelcode@gmail.com>
2022-09-14 15:24:53 +0100

    Setting up models

--- django_tutorial/django_tutorial/settings.py
+++ django_tutorial/django_tutorial/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom_apps
+    'django_models.apps.DjangoModelsConfig',
 ]
 
 MIDDLEWARE = [
