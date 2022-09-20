24c61f228320abdc06ce81a30696f96d15e55e4d
kadelcode <kadelcode@gmail.com>
2022-09-20 21:34:07 +0100

    Adding view_humanize

--- django_tutorial/django_templates/views.py
+++ django_tutorial/django_templates/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import request
 
 # Create your views here.
+def view_humanize(request):
+    data = {
+        "Value1" : 1200000,
+        "Position": 1
+    }
+
+    return render(request, "django_templates\humanize.html", data)
